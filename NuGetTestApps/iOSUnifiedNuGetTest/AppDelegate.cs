5357a0f2826becb1ba4368f673a47173d174299d
Matthew Leibowitz <mattleibow@live.com>
2015-01-15 01:54:30 +0200

    Updated the NuGet test project  - net4 has a versioning issue  - mac can't add the NuGet as XS fails

---
+++ NuGetTestApps/iOSUnifiedNuGetTest/AppDelegate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Mono.Data.Sqlite;
+
+using Foundation;
+using UIKit;
+
+using PortableNuGetTest;
+
+namespace iOSUnifiedNuGetTest
+{
+	[Register("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		UIWindow window;
+		MyViewController viewController;
+
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+			viewController = new MyViewController();
+			window.RootViewController = viewController;
+
+			window.MakeKeyAndVisible();
+
+			var conn = new SqliteConnection(); // we need a reference in order to stop the linker
+
+			var dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "test.db");
+			NuGetTestClass.RunTests(dbPath);
+
+			return true;
+		}
+	}
+}
